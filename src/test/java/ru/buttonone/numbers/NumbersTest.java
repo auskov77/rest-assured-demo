94e5d291db8c6b89d702f72c25172811ec373f61
auskov77 <auskov77@yandex.ru>
2022-08-29 18:36:47 +0300

    add constants headers

--- src/test/java/ru/buttonone/numbers/NumbersTest.java
+++ src/test/java/ru/buttonone/numbers/NumbersTest.java
@@ -2,6 +2,7 @@ package ru.buttonone.numbers;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.http.Header;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,21 @@ public class NumbersTest {
 
     public static final String MAIN_URL = "http://numbersapi.com";
     public static final String NUMBER_URL = "/2";
-    public static final String NUMBERSAPI_COM = "numbersapi.com";
     public static final String DATE_URL = "/8/27/date";
-    public static final String USER_AGENT = "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36";
-    public static final String CONTENT_TYPE = "text/plain; charset=utf-8";
     public static final String MATH_URL = "/8/math";
+    public static final String CONTENT_TYPE = "text/plain; charset=utf-8";
+    Header requestMethod = new Header("Request method", "GET");
+    Header requestUriNumber = new Header("Request URI", MAIN_URL + NUMBER_URL);
+    Header requestUriDate = new Header("Request URI", MAIN_URL + DATE_URL);
+    Header requestUriMath = new Header("Request URI", MAIN_URL + MATH_URL);
+    Header acceptEncoding = new Header("Accept-Encoding", "gzip, deflate");
+    Header acceptLanguage = new Header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
+    Header accept = new Header("Accept", "*/*");
+    Header cacheControl = new Header("Cache-Control", "max-age=0");
+    Header connection = new Header("Connection", "keep-alive");
+    Header host = new Header("Host", "numbersapi.com");
+    Header upgradeInsecureRequests = new Header("Upgrade-Insecure-Requests", "1");
+    Header userAgent = new Header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
 
     @DisplayName("rest assured - " + MAIN_URL + NUMBER_URL)
     @Test
@@ -29,16 +40,16 @@ public class NumbersTest {
         RestAssured
                 .given()
                 .baseUri(MAIN_URL)
-                .header("Request method", "GET")
-                .header("Request URI", MAIN_URL + NUMBER_URL)
-                .header("Accept-Encoding", "gzip, deflate")
-                .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("Accept", "*/*")
-                .header("Cache-Control", "max-age=0")
-                .header("Connection", "keep-alive")
-                .header("Host", NUMBERSAPI_COM)
-                .header("Upgrade-Insecure-Requests", "1")
-                .header("User-Agent", USER_AGENT)
+                .header(requestMethod)
+                .header(requestUriNumber)
+                .header(acceptEncoding)
+                .header(acceptLanguage)
+                .header(accept)
+                .header(cacheControl)
+                .header(connection)
+                .header(host)
+                .header(upgradeInsecureRequests)
+                .header(userAgent)
                 .when()
                 .get(NUMBER_URL)
                 .then()
@@ -63,16 +74,16 @@ public class NumbersTest {
         RestAssured
                 .given()
                 .baseUri(MAIN_URL)
-                .header("Request method", "GET")
-                .header("Request URI", MAIN_URL + DATE_URL)
-                .header("Accept-Encoding", "gzip, deflate")
-                .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("Accept", "*/*")
-                .header("Cache-Control", "max-age=0")
-                .header("Connection", "keep-alive")
-                .header("Host", NUMBERSAPI_COM)
-                .header("Upgrade-Insecure-Requests", "1")
-                .header("User-Agent", USER_AGENT)
+                .header(requestMethod)
+                .header(requestUriDate)
+                .header(acceptEncoding)
+                .header(acceptLanguage)
+                .header(accept)
+                .header(cacheControl)
+                .header(connection)
+                .header(host)
+                .header(upgradeInsecureRequests)
+                .header(userAgent)
                 .when()
                 .get(DATE_URL)
                 .then()
@@ -97,16 +108,16 @@ public class NumbersTest {
         RestAssured
                 .given()
                 .baseUri(MAIN_URL)
-                .header("Request method", "GET")
-                .header("Request URI", MAIN_URL + MATH_URL)
-                .header("Accept-Encoding", "gzip, deflate")
-                .header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-                .header("Accept", "*/*")
-                .header("Cache-Control", "max-age=0")
-                .header("Connection", "keep-alive")
-                .header("Host", NUMBERSAPI_COM)
-                .header("Upgrade-Insecure-Requests", "1")
-                .header("User-Agent", USER_AGENT)
+                .header(requestMethod)
+                .header(requestUriMath)
+                .header(acceptEncoding)
+                .header(acceptLanguage)
+                .header(accept)
+                .header(cacheControl)
+                .header(connection)
+                .header(host)
+                .header(upgradeInsecureRequests)
+                .header(userAgent)
                 .when()
                 .get(MATH_URL)
                 .then()

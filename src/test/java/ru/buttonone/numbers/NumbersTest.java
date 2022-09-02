3087ef2f01b5b3e1c66272a91b818d75bfbc989a
auskov77 <auskov77@yandex.ru>
2022-09-02 14:41:33 +0300

    add RequestSpecification

--- src/test/java/ru/buttonone/numbers/NumbersTest.java
+++ src/test/java/ru/buttonone/numbers/NumbersTest.java
@@ -1,34 +1,28 @@
 package ru.buttonone.numbers;
 
-import io.restassured.RestAssured;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.http.Header;
+import io.restassured.specification.ResponseSpecification;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static ru.buttonone.numbers.specification.NumbersSpecifications.defaultRequestSpecification;
 
 public class NumbersTest {
 
     public static final String MAIN_URL = "http://numbersapi.com";
     public static final String NUMBER_URL = "/2";
-    public static final String DATE_URL = "/8/27/date";
-    public static final String MATH_URL = "/8/math";
-    public static final String CONTENT_TYPE = "text/plain; charset=utf-8";
+    public static final String NUMBER_URL_2 = "http://numbersapi.com/{id}";
     public static final Header requestMethod = new Header("Request method", "GET");
-    public static final Header requestUriNumber = new Header("Request URI", MAIN_URL + NUMBER_URL);
-    public static final Header requestUriDate = new Header("Request URI", MAIN_URL + DATE_URL);
-    public static final Header requestUriMath = new Header("Request URI", MAIN_URL + MATH_URL);
-    public static final Header acceptEncoding = new Header("Accept-Encoding", "gzip, deflate");
-    public static final Header acceptLanguage = new Header("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7");
-    public static final Header accept = new Header("Accept", "*/*");
-    public static final Header cacheControl = new Header("Cache-Control", "max-age=0");
-    public static final Header connection = new Header("Connection", "keep-alive");
-    public static final Header host = new Header("Host", "numbersapi.com");
-    public static final Header upgradeInsecureRequests = new Header("Upgrade-Insecure-Requests", "1");
-    public static final Header userAgent = new Header("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36");
+    public static final String ID_PATH = "/{id}";
 
-    @DisplayName("rest assured - " + MAIN_URL + NUMBER_URL)
+
+    @DisplayName("rest assured - " + MAIN_URL + NUMBER_URL_2)
     @Test
-    public void shouldHaveCorrectGetNumberUrl() {
+    public void shouldHaveCorrectGet1() {
 
 //        Response response = RestAssured.given().get("http://numbersapi.com/2");
 //
@@ -37,103 +31,42 @@ public class NumbersTest {
 //
 //        Assertions.assertEquals(200, response.statusCode());
 
-        RestAssured
-                .given()
+        given()
                 .baseUri(MAIN_URL)
                 .header(requestMethod)
-                .header(requestUriNumber)
-                .header(acceptEncoding)
-                .header(acceptLanguage)
-                .header(accept)
-                .header(cacheControl)
-                .header(connection)
-                .header(host)
-                .header(upgradeInsecureRequests)
-                .header(userAgent)
                 .when()
                 .get(NUMBER_URL)
                 .then()
                 .contentType(ContentType.TEXT)
                 .log().all()
                 .header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
-                .header("Expires", "0")
-                .header("Pragma", "no-cache")
-                .header("X-Numbers-API-Type", "trivia")
-                .header("X-Numbers-API-Number", "2")
-                .header("Server", "nginx/1.4.6 (Ubuntu)")
-                .header("X-Powered-By", "Express")
-                .header("Connection", "keep-alive")
-                .header("Content-Type", CONTENT_TYPE)
-                .header("Access-Control-Allow-Origin", "*")
                 .statusCode(200);
+
     }
 
-    @DisplayName("rest assured - " + MAIN_URL + DATE_URL)
     @Test
-    public void shouldHaveCorrectGetDateUrl() {
-        RestAssured
-                .given()
-                .baseUri(MAIN_URL)
-                .header(requestMethod)
-                .header(requestUriDate)
-                .header(acceptEncoding)
-                .header(acceptLanguage)
-                .header(accept)
-                .header(cacheControl)
-                .header(connection)
-                .header(host)
-                .header(upgradeInsecureRequests)
-                .header(userAgent)
-                .when()
-                .get(DATE_URL)
-                .then()
-                .contentType(ContentType.TEXT)
-                .log().all()
-                .header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
-                .header("Expires", "0")
-                .header("Pragma", "no-cache")
-                .header("X-Numbers-API-Type", "date")
-                .header("X-Numbers-API-Number", "240")
-                .header("Server", "nginx/1.4.6 (Ubuntu)")
-                .header("X-Powered-By", "Express")
-                .header("Connection", "keep-alive")
-                .header("Content-Type", CONTENT_TYPE)
-                .header("Access-Control-Allow-Origin", "*")
-                .statusCode(200);
-    }
+    public void shouldHaveCorrectGet2() {
 
-    @DisplayName("rest assured - " + MAIN_URL + MATH_URL)
-    @Test
-    public void shouldHaveCorrectGetMathUrl() {
-        RestAssured
-                .given()
-                .baseUri(MAIN_URL)
-                .header(requestMethod)
-                .header(requestUriMath)
-                .header(acceptEncoding)
-                .header(acceptLanguage)
-                .header(accept)
-                .header(cacheControl)
-                .header(connection)
-                .header(host)
-                .header(upgradeInsecureRequests)
-                .header(userAgent)
+//        RequestSpecification requestSpecification
+//                = new RequestSpecBuilder()
+//                .addHeader("Accept-Language", "ru")
+//                .setBaseUri(MAIN_URL + NUMBER_URL)
+//                .build();
+
+        ResponseSpecification responseSpecification
+                = new ResponseSpecBuilder()
+                .log(LogDetail.ALL)
+                .expectStatusCode(200)
+                .build();
+
+        given()
+                .spec(defaultRequestSpecification())
+//                .pathParam("id", 2)
                 .when()
-                .get(MATH_URL)
+                .get(ID_PATH)
                 .then()
                 .contentType(ContentType.TEXT)
-                .log().all()
-                .header("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0")
-                .header("Expires", "0")
-                .header("Pragma", "no-cache")
-                .header("X-Numbers-API-Type", "math")
-                .header("X-Numbers-API-Number", "8")
-                .header("Server", "nginx/1.4.6 (Ubuntu)")
-                .header("X-Powered-By", "Express")
-                .header("Connection", "keep-alive")
-                .header("Content-Type", CONTENT_TYPE)
-                .header("Access-Control-Allow-Origin", "*")
-                .statusCode(200);
+                .spec(responseSpecification);
     }
 
 }
